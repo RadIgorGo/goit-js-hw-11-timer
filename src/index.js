91e1c5328b26f238cc944360bbe126344374de6a
RadIgorGo <70336756+RadIgorGo@users.noreply.github.com>
2021-05-02 14:06:34 +0300

    npm

--- src/index.js
+++ src/index.js
@@ -1,67 +1,50 @@
 import './styles.css';
 
 const refs = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]'),
+  days: document.querySelector('.value[data-value="days"]'),
+  hours: document.querySelector('.value[data-value="hours"]'),
+  mins: document.querySelector('.value[data-value="mins"]'),
+  secs: document.querySelector('.value[data-value="secs"]'),
 };
-console.log(refs);
-const timer = {
-  start() {
-    const startTime = Date.now();
-
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
-      console.log('start->currentTime', currentTime);
-
-      console.log(`${days}::${hours}::${mins}::${secs}`);
-    }, 1000);
-  },
-};
-timer.start();
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-
-function getTimeComponents(time) {
+//Для подсчета значений используй следующие готовые формулы, где time - разница между targetDate и текущей датой.
+function getTimer(time) {
   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
   const hours = pad(
     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
   );
   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
   return { days, hours, mins, secs };
 }
-// new CountdownTimer({
-//   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
-// });
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-//const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-//const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+class СountdownTimer {
+  constructor({ targetDate }) {
+    this.targetDate = targetDate;
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-//const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    this.start = function () {
+      const startTime = targetDate;
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-//const secs = Math.floor((time % (1000 * 60)) / 1000);
+      setInterval(() => {
+        const currentTime = Date.now();
+        const deltaTime = startTime - currentTime;
+        const { days, hours, mins, secs } = getTimer(deltaTime);
+        refs.days.innerText = days;
+        refs.hours.innerText = hours;
+        refs.mins.innerText = mins;
+        refs.secs.innerText = secs;
+      }, 1000);
+    };
+  }
+}
+
+const deadLine = new СountdownTimer({
+  targetDate: new Date('05, 07, 2021'),
+});
+
+deadLine.start();
